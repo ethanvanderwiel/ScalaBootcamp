1bbe7d68e5c8c3d321e40946ecb9875157c2e976
Ethan Vander Wiel <ethan.vanderwiel@banno.com>
2019-06-25 10:50:55 -0500

    Updated AbstractImpl code for testing

--- IterationThree/src/main/scala/AbstractImpl.scala
+++ IterationThree/src/main/scala/AbstractImpl.scala
@@ -1,8 +1,14 @@
-object AbstractImpl {
-    sealed abstract class RaceCar() {
+    sealed abstract class RaceCar {
         def driver: String
         def startEngine: String = s"${driver}'s Engine started! Ready to race" //I'm aware all of these defs should probably be vals. 
         def pitStop: String = s"${driver} is in the pit lane! Car: Up! Tires: Changed! Car: Down! Go!"
+    }
+
+    object RaceCarFactory {
+        def apply(s: String): RaceCar = {
+            if(s == "F1Car") F1Car("Lando Norris")
+            else IndyCar("Alex Rossi")
+        }
     }
 
     case class F1Car(driver: String) extends RaceCar {
@@ -13,15 +19,20 @@ object AbstractImpl {
         def refuel = s"${driver} is refueling...."
     }
 
+object AbstractImpl {
+    
+
+    
+
     def main(args: Array[String]) {
-        val Norris = F1Car("Lando Norris")
-        val Rossi = IndyCar("Alex Rossi")
+        val Norris = RaceCarFactory("F1Car")
+        val Rossi = RaceCarFactory("IndyCar")
 
         println(Norris.startEngine) //Lando Norris's Engine started! Ready to race
         println(Rossi.startEngine) //Alex Rossi's Engine started! Ready to race
         println(Rossi.pitStop) //Alex Rossi is in the pit lane! Car: Up! Tires: Changed! Car: Down! Go!
-        println(Norris.drsStart) //Lando Norris opens the hatch: going fast!
-        println(Rossi.refuel) //Alex Rossi is refueling....
+        println(Norris.drsStart()) //Lando Norris opens the hatch: going fast!
+        println(IndyCarRossi).refuel) //Alex Rossi is refueling....
         
     }
 
