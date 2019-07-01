f2a4b34613b896e6aa6acb7314f04988d6301fe9
Ethan Vander Wiel <ethan.vanderwiel@banno.com>
2019-07-01 13:42:17 -0500

    Made case classes extend the sealed trait

--- IterationThree/src/main/scala/SealedTrait.scala
+++ IterationThree/src/main/scala/SealedTrait.scala
@@ -1,17 +1,17 @@
 object SealedTraits {
     sealed trait Values
 
-    case class Numbers(i: Int)
-    case class Strings(s: String)
-    case class UnaryFunction(f: Any => Any)
+    case class Numbers(i: Int) extends Values
+    case class Strings(s: String) extends Values
+    case class UnaryFunction(f: Any => Any) extends Values
 
-    def interpret(a: Any): String = {
-        a match {
+    def interpret(v: Values): String = {
+        v match {
             case x: Numbers if x.i < 10 => s"This is a number less than 10: ${x.i}"
-            case x:Numbers => s"This is a number 10 or greater: ${x.i}"
-            case x:Strings => s"This is a string: ${x.s}"
-            case x:UnaryFunction => s"This is a unary function: ${x.f}" //Unsure if there is a way to see function body
-            case _ => s"Uh oh, this is an unexpected result: ${a}"
+            case x: Numbers => s"This is a number 10 or greater: ${x.i}"
+            case x: Strings => s"This is a string: ${x.s}"
+            case x: UnaryFunction => s"This is a unary function: ${x.f}" //Unsure if there is a way to see function body
+            case _ => s"Uh oh, this is an unexpected result: ${v}"
         }
     }
 
@@ -25,10 +25,10 @@ object SealedTraits {
         def +(b: Any):Any = b
         val u1 = UnaryFunction(+) 
 
-        val ux = 3
+        //val ux = 3
         
 
-        val col = List(n1, n2, s1, s2, u1, ux)
+        val col = List(n1, n2, s1, s2, u1)
         col foreach { x => println(interpret(x)) }
     }
 }
